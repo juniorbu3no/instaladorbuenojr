90fac9ddd245e2406a04635db0090ec931acd14f
juniorbu3no <junior@buenojr.com.br>
2024-03-27 20:33:16 -0300

    Instalador
    Instalador

---
+++ utils/_banner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+  printf "${NC}";
+
+  printf "\n"
+
+printf "${GREEN}";  
+printf "${NC}";
+  
+  
+
+  printf "\n"
+}
